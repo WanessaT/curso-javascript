739b56c79d4a05261b337607912ca5cabe8c330e
WanessaT <wanessatdev@gmail.com>
2023-11-08 19:06:16 -0300

    Aprendendo sobre if e else

---
+++ aula-11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+console.log(`Dirija sempre usando cinto de segurança`)
+if (vel > 60) { //Condição simples
+    console.log(`Você ultrapassou o limite de velocidade. Multado!`)
+}
