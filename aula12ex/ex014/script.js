552e59e0010679595af44b654c292f7cccddc16e
WanessaT <wanessatdev@gmail.com>
2023-11-18 19:38:18 -0300

    Exercício hora do dia

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,1 +1,17 @@
-alert('Olá!')
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        img.src = 'imagens/manha.png'
+        document.body.style.background = '#F1C987'
+    } else if (hora >= 12 && hora < 18) {
+        img.src = 'imagens/tarde.png'
+        document.body.style.background = '#B94901'
+    } else {
+        img.src = 'imagens/noite.png'
+        document.body.style.background = '#101F39'
+    }
+}
